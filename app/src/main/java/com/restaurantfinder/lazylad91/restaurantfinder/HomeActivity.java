ab0fa2a3f1d084b4ff91776029b8d8f4031f6919
hanamichizz <30983981+hanamichizz@users.noreply.github.com>
2018-05-05 00:10:42 +0700

    Update last

--- app/src/main/java/com/restaurantfinder/lazylad91/restaurantfinder/HomeActivity.java
+++ app/src/main/java/com/restaurantfinder/lazylad91/restaurantfinder/HomeActivity.java
@@ -80,7 +80,6 @@ public class HomeActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
